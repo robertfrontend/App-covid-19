5c7f83ba391156dc0feafa0cb7565614d733c108
Robert Romero Medina <enanow41romero@gmail.com>
2020-05-18 19:53:47 -0400

    algunos cambios

--- Js/app.js
+++ Js/app.js
@@ -1,7 +1,31 @@
+const linkTotal = 'https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/brief';
+
+fetch(linkTotal)
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        htmlTotal = '';        
+        htmlTotal += `
+                <p class="text-warning" >
+                    <span>Confirmados:</span>
+                    ${data.confirmed}
+                </p>
+                <p class="text-danger">
+                    <span>Muertes:</span>
+                    ${data.deaths}
+                </p>
+                <p class="text-success" >
+                    <span>Recuperados:</span>
+                    ${data.recovered}
+                </p>
+        `;
+        document.querySelector('.total').innerHTML = htmlTotal;
+    });
 
 
+//link de la api
 const linkApi = 'https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/latest'
-
 
 // llamar api
 fetch(linkApi)
@@ -13,39 +37,26 @@ fetch(linkApi)
 
         datos.forEach(paises => {
             htmlRes += `
-                <div class="card text-white" id="tarjeta" >
+                <div class="card text-dark"  id="tarjeta" >
                     <div class="card-body">
                         <div >
                             <h4 class="card-title">
                                 <span class="">
-                                    Pais:
                                 </span>
                                 ${paises.countryregion}
-                            </h4>
-                            <h5 class="card-title">
-                                <span class="">
-                                    Estado/Provincia:
-                                </span>
-                                ${paises.provincestate}
-                            </h5>
-                            <h6 class="card-subtitle mb-2">
-                                <span class="text-white">
-                                    Ultima actualizacion:
-                                </span>
-                                ${paises.lastupdate}
-                            </h6>                        
+                            </h4>                     
                         </div>
                         <div class="" id="da">
-                            <p class="card-text m-0 text-warning ">
-                                <span class=" text-white">Confirmados:</span>
+                            <p class="card-text text-warning ">
+                                <span class="">Confirmados:</span>
                                 ${paises.confirmed} 
                             </p>
-                            <p class="card-text m-0 text-danger">
-                                <span class=" text-white">Muertes: </span>
+                            <p class="card-text text-danger">
+                                <span class="">Muertes: </span>
                                 ${paises.deaths} 
                             </p>
-                            <p class="card-text r m-0 text-success " >
-                                <span class=" text-white">Recuperados:</span>
+                            <p class="card-text  text-success " >
+                                <span class="">Recuperados:</span>
                                 ${paises.recovered} 
                             </p>
                         </div>
@@ -53,11 +64,9 @@ fetch(linkApi)
                 </div>
             
             `;
-        })
-
-        document.querySelector('.paises').innerHTML = htmlRes
-
-    })
+        });
+        document.querySelector('.paises').innerHTML = htmlRes;
+    });
 
 
 
@@ -84,22 +93,9 @@ function domRespuesta() {
                         <div >
                             <h4 class="card-title">
                                 <span class="">
-                                    Pais:
                                 </span>
                                 ${domi.countryregion}
-                            </h4>
-                            <h5 class="card-title">
-                                <span class="">
-                                    Estado/Provincia:
-                                </span>
-                                ${domi.provincestate}
-                            </h5>
-                            <h6 class="card-subtitle mb-2">
-                                <span class="text-white">
-                                    Ultima actualizacion:
-                                </span>
-                                ${domi.lastupdate}
-                            </h6>                        
+                            </h4>                      
                         </div>
                         <div class="" id="da">
                             <p class="card-text m-0 text-warning ">
