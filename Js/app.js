3677bfff1d326b9f82c9ca8f8e2dc90128c9b703
Robert Romero Medina <enanow41romero@gmail.com>
2020-05-20 23:53:04 -0400

    agregando filtrador de paises

--- Js/app.js
+++ Js/app.js
@@ -3,132 +3,102 @@ const dcarga = `
 `;
 
 
-const linkTotal = 'https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/brief';
 window.onload = () => {
-    document.querySelector('.d-carga ').innerHTML = dcarga
+    
+    // mostrar el total del mundo
+
+    // link de la api
+    const linkTotal = 'https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/brief';
+    // spiner de carga
+    document.querySelector('.d-carga ').innerHTML = dcarga;
+
     fetch(linkTotal)
         .then(res => {
             return res.json();
         })
         .then(data => {
-            htmlTotal = '';        
+
+            const {confirmed, deaths, recovered} = data
+            htmlTotal = '';
+
+            // reteenmos los datos        
             htmlTotal += `
                     <div>
                         <p class="text-warning">
-                            ${Intl.NumberFormat().format(data.confirmed)}
+                            ${Intl.NumberFormat().format(confirmed)}
                         </p>
                         <h3>Casos Confirmados</h3>
                     </div>
                     <div>
                         <p class="text-danger" >
-                            ${Intl.NumberFormat().format(data.deaths)}
+                            ${Intl.NumberFormat().format(deaths)}
                         </p>
                         <h3>Muertes</h3>
     
                     </div>
                     <div>
                         <p class="text-success" >
-                        ${Intl.NumberFormat().format(data.recovered)}
+                        ${Intl.NumberFormat().format(recovered)}
                         </p>
                         <h3>Recuperados</h3>
                     </div>
             `;
+            // mandamos los datos al html
             document.querySelector('.total').innerHTML = htmlTotal;
         });
+
+
+    // mostrar todos los paises
+    todosLosPaises();
+    
+    function todosLosPaises() {
+
+        //link de la api
+        const linkApi = 
+        `https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/latest`;
+        // llamar api
+        fetch(linkApi)
+            .then(respuesta => {
+                return respuesta.json();
+            })
+            .then(datos => {
+                htmlRes = ''
+                datos.forEach(paises => {
+    
+                    const {countryregion, confirmed, deaths, recovered} = paises;
+    
+                    htmlRes += `
+                        <div class="card text-dark" id="tarjeta" >
+                            <div class="card-body">
+                                <div >
+                                    <h4 class="card-title">
+                                        <span class="">
+                                        </span>
+                                        ${countryregion}
+                                    </h4>                     
+                                </div>
+                                <div class="" id="da">
+                                    <p class="card-text text-warning ">
+                                        <span class="text-light">Confirmados:</span>
+                                        ${Intl.NumberFormat().format(confirmed)} 
+                                    </p>
+                                    <p class="card-text text-danger">
+                                        <span class="text-light">Muertes: </span>
+                                        ${Intl.NumberFormat().format(deaths)} 
+                                    </p>
+                                    <p class="card-text  text-success " >
+                                        <span class="text-light">Recuperados:</span>
+                                        ${Intl.NumberFormat().format(recovered)} 
+                                    </p>
+                                </div>
+                            </div>
+                        </div>          
+                    `;
+                });
+                document.querySelector('.resultados-paises').innerHTML = htmlRes;
+                document.querySelector('.d-carga ').innerHTML = ''
+    
+            });
+    }// fin function todosLosPaises
 }
 
-
- //link de la api
- const linkApi = 'https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/latest'
- // llamar api
- fetch(linkApi)
-     .then(respuesta => {
-         return respuesta.json();
-     })
-     .then(datos => {
-         htmlRes = ''
-         datos.forEach(paises => {
-             htmlRes += `
-                 <div class="card text-dark"  id="tarjeta" >
-                     <div class="card-body">
-                         <div >
-                             <h4 class="card-title">
-                                 <span class="">
-                                 </span>
-                                 ${paises.countryregion}
-                             </h4>                     
-                         </div>
-                         <div class="" id="da">
-                             <p class="card-text text-warning ">
-                                 <span class="text-light">Confirmados:</span>
-                                 ${Intl.NumberFormat().format(paises.confirmed)} 
-                             </p>
-                             <p class="card-text text-danger">
-                                 <span class="text-light">Muertes: </span>
-                                 ${Intl.NumberFormat().format(paises.deaths)} 
-                             </p>
-                             <p class="card-text  text-success " >
-                                 <span class="text-light">Recuperados:</span>
-                                 ${Intl.NumberFormat().format(paises.recovered)} 
-                             </p>
-                         </div>
-                     </div>
-                 </div>          
-             `;
-         });
-         document.querySelector('.resultados-paises').innerHTML = htmlRes;
-         document.querySelector('.d-carga ').innerHTML = ''
-
-     });
-
-
-
-//     // <a href="#" class="card-link">Card link</a>
-//     // <a href="#" class="card-link">Another link</a>
-
-// const btnDominicana = document.querySelector('#btnDom')
-// btnDominicana.addEventListener('click', domRespuesta)
-
-// const linkDom = 'https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/latest?iso2=DO&iso3=DOM'
-
-// function domRespuesta() {
-//     fetch(linkDom)
-//         .then(respuesta => {
-//             return respuesta.json();
-//         } )
-//         .then(data => {
-//             let htmlDOP = '';
-
-//             data.forEach(domi => {
-//                 htmlDOP += `
-//                     <div class="card text-white" id="tarjeta" >
-//                     <div class="card-body">
-//                         <div >
-//                             <h4 class="card-title">
-//                                 <span class="">
-//                                 </span>
-//                                 ${domi.countryregion}
-//                             </h4>                      
-//                         </div>
-//                         <div class="" id="da">
-//                             <p class="card-text m-0 text-warning ">
-//                                 <span class=" text-white">Confirmados:</span>
-//                                 ${domi.confirmed} 
-//                             </p>
-//                             <p class="card-text m-0 text-danger">
-//                                 <span class=" text-white">Muertes: </span>
-//                                 ${domi.deaths} 
-//                             </p>
-//                             <p class="card-text r m-0 text-success " >
-//                                 <span class=" text-white">Recuperados:</span>
-//                                 ${domi.recovered} 
-//                             </p>
-//                         </div>
-//                     </div>
-//                 </div>
-//                 `;
-//             }) 
-//             document.querySelector('.paises').innerHTML = htmlDOP
-//         })
-
-// }
---
+++ Js/select.js
@@ -0,0 +1,80 @@
+const apiSelect = 'https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/latest';
+fetch(apiSelect)
+    .then(res => {
+        return res.json()
+    })
+    .then(datos => {
+        
+        htmlSelect = '';
+        
+        datos.forEach(paises => {
+            const {countryregion, countrycode, confirmed } = paises
+            if(countrycode !== undefined) {
+                htmlSelect += `
+                    <option id="" value='${countrycode["iso2"]},${countrycode["iso3"]}'>
+                    ${countryregion}
+                    </option>
+                `
+            }
+        })
+
+        document.getElementById('selecionPaises').innerHTML = htmlSelect
+
+        const select = document.querySelector('#selecionPaises')
+        select.addEventListener('change', () => {
+            const valorSelect = select.value;
+
+            valoresISO =valorSelect.split(',')
+
+            valorISO1 = valoresISO[0]
+            valorISO2 = valoresISO[1]
+            const linkBusc = 
+            `https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/latest?iso2=${valorISO1}&iso3=${valorISO2}`
+            fetch(linkBusc)
+            .then(respuesta => {
+                return respuesta.json()
+            })
+            .then(data => {
+                let htmlResultss = '';
+
+                data.forEach(resultPais => {
+                    const {countryregion, countrycode, confirmed,deaths,recovered,
+                        provincestate} = resultPais
+                            htmlResultss += `
+                            <div class="card text-dark" id="tarjeta" >
+                            <div class="card-body">
+                                <div >
+                                    <h4 class="card-title">
+                                        <span class="">
+                                        </span>
+                                        ${countryregion}
+                                    </h4>
+                                    <h5>
+                                        <span>
+                                            Provincia/Estado
+                                        </span>
+                                        ${provincestate}
+                                    </h5>                     
+                                </div>
+                                <div class="" id="da">
+                                    <p class="card-text text-warning ">
+                                        <span class="text-light">Confirmados:</span>
+                                        ${Intl.NumberFormat().format(confirmed)} 
+                                    </p>
+                                    <p class="card-text text-danger">
+                                        <span class="text-light">Muertes: </span>
+                                        ${Intl.NumberFormat().format(deaths)} 
+                                    </p>
+                                    <p class="card-text  text-success " >
+                                        <span class="text-light">Recuperados:</span>
+                                        ${Intl.NumberFormat().format(recovered)} 
+                                    </p>
+                                </div>
+                            </div>
+                        </div>                          
+                    `;
+                })
+                document.querySelector('.resultados-paises').innerHTML = htmlResultss;
+            })
+        } )
+    })
